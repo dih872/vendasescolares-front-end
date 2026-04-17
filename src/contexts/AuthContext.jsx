ce92ab0c0e39a3b9c175a6e082aac5c27062c9e2
Diego Santana <ds1368833@gmail.com>
2026-04-16 22:09:35 -0300

    Coreção

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -32,7 +32,7 @@ export function AuthProvider({ children }) {
   }, []);
 
   const login = async (email, senha) => {
-    const response = await api.post('/api/auth/login', { email, senha });
+    const response = await api.post('/auth/login', { email, senha });
     const { token, ...usuario } = response.data;
     
     localStorage.setItem('adminToken', token);
