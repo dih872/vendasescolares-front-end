16117c3ff13a027c28aca0211ee7aa540ce5df34
Diego Santana <ds1368833@gmail.com>
2026-04-16 21:55:47 -0300

    coreção

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL
+  baseURL: import.meta.env.VITE_API_URL
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('adminToken');
